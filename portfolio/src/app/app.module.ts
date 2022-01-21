dc513c5b43eb89e6e3bc8e0c2b991bfb2a61859f
meyer <jrm533@psu.edu>
2022-01-20 20:41:01 -0500

    basic styling. Added svgIcon set

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ApplicationRef, NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,8 +18,26 @@ import { HomeComponent } from './home/home.component';
     MatToolbarModule,
     MatCardModule,
     MatButtonModule,
+    MatIconModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [MatIconRegistry],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(
+    public appRef: ApplicationRef,
+    matIconRegistry: MatIconRegistry,
+    domSanitizer: DomSanitizer
+  ) {
+    // matIconRegistry.addSvgIconLiteralInNamespace(
+    //   'svgIcons',
+    //   'svgIcon',
+    //   domSanitizer.bypassSecurityTrustHtml('<svg></svg>')
+    // );
+
+    matIconRegistry.addSvgIconSet(
+      domSanitizer.bypassSecurityTrustResourceUrl('/assets/mdi.svg')
+    ); // Or whatever path you placed mdi.svg at
+  }
+}
